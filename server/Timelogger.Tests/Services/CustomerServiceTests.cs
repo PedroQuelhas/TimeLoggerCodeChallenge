952f0d4910ed103474926525a6050fbceb05b000
Pedro Quelhas <pedro@leadrover.com>
2023-05-08 22:42:32 +0100

    Added a couple of unit tests

---
+++ server/Timelogger.Tests/Services/CustomerServiceTests.cs
@@ -0,0 +1,91 @@
+﻿using AutoMapper;
+using FakeItEasy;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ServerApi.CodeGen.Models;
+using System;
+using System.Threading.Tasks;
+using Timelogger.Model;
+using Timelogger.Repos;
+using Timelogger.Services;
+using FluentAssertions;
+
+
+namespace Timelogger.Tests.Services
+{
+    [TestClass]
+    public class CustomerServiceTests
+    {
+
+        [TestMethod]
+        public async Task CreateCustomer_NewUser_Success()
+        {
+            var fakeRepo = A.Fake<ICustomerRepository>();
+            var fakeMapper = A.Fake<IMapper>();
+
+            var id = Guid.NewGuid();
+            var customerDto = new CustomerDTO
+            {
+                Name = "TestName",
+            };
+
+            var customerDtoWithId = new CustomerDTO
+            {
+                Id = id,
+                Name = "TestName",
+            };
+
+            var customer = new Customer
+            {
+                Name = "TestName",
+            };
+
+            var customerWithId = new Customer
+            {
+                ID = id,
+                Name = "TestName",
+            };
+
+            A.CallTo(() => fakeMapper.Map<Customer>(customerDto)).Returns(customer);
+            A.CallTo(() => fakeRepo.Create(customer)).Returns(customerWithId);
+            A.CallTo(() => fakeMapper.Map<CustomerDTO>(customerWithId)).Returns(customerDtoWithId);
+
+            var service = new CustomerService(fakeRepo, fakeMapper);
+            var result = await service.Create(customerDto);
+
+            A.CallTo(() => fakeMapper.Map<Customer>(customerDto)).MustHaveHappenedOnceExactly();
+            A.CallTo(() => fakeRepo.Create(customer)).MustHaveHappenedOnceExactly();
+            A.CallTo(() => fakeMapper.Map<CustomerDTO>(customerWithId)).MustHaveHappenedOnceExactly();
+
+            customerDtoWithId.Should().BeEquivalentTo(result);
+        }
+
+        [TestMethod]
+        public void CreateCustomer_UserExists_Fail()
+        {
+            var fakeRepo = A.Fake<ICustomerRepository>();
+            var fakeMapper = A.Fake<IMapper>();
+
+            var id = Guid.NewGuid();
+            var customerDto = new CustomerDTO
+            {
+                Name = "TestName",
+            };
+
+            var customer = new Customer
+            {
+                Name = "TestName",
+            };
+
+            var msg = "USER ALREADY EXISTS";
+            A.CallTo(() => fakeMapper.Map<Customer>(customerDto)).Returns(customer);
+            A.CallTo(() => fakeRepo.Create(customer)).ThrowsAsync(new DbUpdateException(msg));
+
+            var service = new CustomerService(fakeRepo, fakeMapper);
+            Assert.ThrowsExceptionAsync<DbUpdateException>(() => service.Create(customerDto), msg);
+
+            A.CallTo(() => fakeMapper.Map<Customer>(customerDto)).MustHaveHappenedOnceExactly();
+            A.CallTo(() => fakeRepo.Create(customer)).MustHaveHappenedOnceExactly();
+        }
+    }
+}
---
+++ server/Timelogger.Tests/Services/ProjectServiceTests.cs
@@ -0,0 +1,149 @@
+﻿using AutoMapper;
+using FakeItEasy;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ServerApi.CodeGen.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Timelogger.Model;
+using Timelogger.Repos;
+using Timelogger.Services;
+using FluentAssertions;
+
+namespace Timelogger.Tests.Services
+{
+    [TestClass]
+    public class ProjectServiceTests
+    {
+        [TestMethod]
+        public async Task GetProjectsOverview_AllProjectsNoFiltersNoSorting_Success()
+        {
+            var fakeRepo = A.Fake<IProjectRepository>();
+            var fakeRimeslotRepo = A.Fake<ITimeslotRepository>();
+            var fakeMapper = A.Fake<IMapper>();
+
+            var projects = GetProjects();
+            var results = GetProjectResults();
+
+            A.CallTo(() => fakeRepo.GetAllProjectsExpanded(null, null, null, null, "ID", SortOrder.DESC)).Returns((projects, 2));
+            
+            var paginationDTO = new PaginationDTO
+            {
+                Page = 0,
+                PerPage = 0,
+                TotalRecords = 2
+            };
+
+            var service = new ProjectService(fakeRepo, fakeRimeslotRepo, fakeMapper);
+            var result = await service.GetProjectsOverview(null, null, null, null, null, null);
+            result.pagination.Should().BeEquivalentTo(paginationDTO);
+            result.data.ToList().Should().BeEquivalentTo(results);
+        }
+
+        private List<ProjectReportDTO> GetProjectResults() => new List<ProjectReportDTO>
+        {
+            new ProjectReportDTO
+            {
+                ProjectId=Guid.Parse("bfa1cd55-3d06-4327-b7bb-5925bba4017e"),
+                ProjectName="Test1",
+                Completed = false,
+                Deadline ="08/01/2023 00:00:00 +00:00",
+                EndDate = "06/01/2023 00:00:00 +00:00",
+                StartDate = "01/01/2023 00:00:00 +00:00",
+                CustomerName="customer1",
+                TotalRecords=2,
+                TotalTime=90
+            },
+             new ProjectReportDTO
+            {
+                ProjectId=Guid.Parse("9135c6f7-4339-4742-a2ae-20acebfb3458"),
+                ProjectName="Test2",
+                Completed = true,
+                Deadline = "07/01/2023 00:00:00 +00:00",
+                EndDate = "12/01/2023 00:00:00 +00:00",
+                StartDate = "02/01/2023 00:00:00 +00:00",
+                CustomerName="customer2",
+                TotalRecords=3,
+                TotalTime=270
+            },
+        };
+
+
+        private List<Project> GetProjects() => new List<Project>
+        {
+            new Project
+            {
+                ID = Guid.Parse("bfa1cd55-3d06-4327-b7bb-5925bba4017e"),
+                StartDate = DateTimeOffset.Parse("01-01-2023"),
+                EndDate = DateTimeOffset.Parse("01-01-2023").AddDays(5),
+                CustomerId = Guid.NewGuid(),
+                Completed = false,
+                Deadline = DateTimeOffset.Parse("01-01-2023").AddDays(7),
+                Name = "Test1",
+                Timeslots = new List<Timeslot>
+                {
+                    new Timeslot
+                    {
+                          ID = Guid.NewGuid(),
+                        Duration = TimeSpan.FromMinutes(30),
+                        ProjectId = Guid.NewGuid(),
+                        StartTime = DateTimeOffset.Now,
+                    },
+                      new Timeslot
+                    {
+                         ID = Guid.NewGuid(),
+                        Duration = TimeSpan.FromMinutes(60),
+                        ProjectId = Guid.NewGuid(),
+                        StartTime = DateTimeOffset.Now,
+                    },
+                },
+                Customer = new Customer
+                {
+                    ID = Guid.Parse("78d491af-5c2c-4faa-9cc6-c232e56165d0"),
+                    Name="customer1"
+                }
+            },
+            new Project
+            {
+                ID = Guid.Parse("9135c6f7-4339-4742-a2ae-20acebfb3458"),
+                StartDate = DateTimeOffset.Parse("02-01-2023"),
+                EndDate = DateTimeOffset.Parse("02-01-2023").AddDays(10),
+                CustomerId = Guid.NewGuid(),
+                Completed = true,
+                Deadline = DateTimeOffset.Parse("02-01-2023").AddDays(5),
+                Name = "Test2",
+                Timeslots = new List<Timeslot>
+                {
+                    new Timeslot
+                    {
+                        ID = Guid.NewGuid(),
+                        Duration = TimeSpan.FromMinutes(60),
+                        ProjectId = Guid.NewGuid(),
+                        StartTime = DateTimeOffset.Now,
+                    },
+                      new Timeslot
+                    {
+                        ID = Guid.NewGuid(),
+                        Duration = TimeSpan.FromMinutes(90),
+                        ProjectId = Guid.NewGuid(),
+                        StartTime = DateTimeOffset.Now,
+                    },
+                       new Timeslot
+                    {
+                        ID = Guid.NewGuid(),
+                        Duration = TimeSpan.FromMinutes(120),
+                        ProjectId = Guid.NewGuid(),
+                        StartTime = DateTimeOffset.Now,
+                    },
+                },
+                Customer = new Customer
+                {
+                    ID = Guid.Parse("c0c67e74-457f-4e45-973d-3d584a91ce2a"),
+                    Name="customer2"
+                }
+            }
+        };
+    }
+}
